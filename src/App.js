9e941f1cd13462155e122ac1ac2fe9c88e9e48a5
JEONG JAEEUN <jje@sookmyung.ac.kr>
2023-04-08 19:54:48 +0900

    프론트 개발2. Props, state 사용하기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import logo from './logo.svg';
 import React from 'react';
-import Hello from './Hello';
+import Todo from './Todo';
 import './App.css';
-
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      item:{id:0, title:"Hello World 1", done:true},
+  };
+}
   render(){
-    return (
+    return(
       <div className="App">
-        <Hello />
+        <Todo item={this.state.item}/>
       </div>
     );
   }
 }
-
 export default App;
