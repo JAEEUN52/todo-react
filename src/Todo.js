2bbd17f7bcddd9d0f8d473da33348591011a3fab
JEONG JAEEUN <jje@sookmyung.ac.kr>
2023-04-09 19:51:07 +0900

    프론트 개발5. React 이벤트 핸들러로 목록 추가하기

--- src/Todo.js
+++ src/Todo.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import { ListItem, ListItemText, InputBase, Checkbox } from "@material-ui/core";
+import {ListItem, ListItemText, InputBase, Checkbox} from "@material-ui/core";
+
 class Todo extends React.Component {
-    constructor(props) {
+    constructor(props){
         super(props);
-        this.state = { item: props.item };
+        this.state ={ item: props.item };  // 매개변수 item 의 변수/값을 item에 대입
     }
-    render() {
-        const item = this.state.item;
+
+    render(){
+        const item =this.state.item;
         return (
             <ListItem>
-                <Checkbox checked={item.done} />
+                <Checkbox checked={item.done}/>
                 <ListItemText>
                     <InputBase
-                        inputProps={{ "aria-label": "naked" }}
-                        type="text"
-                        id={item.id}
-                        name={item.id}
-                        value={item.title}
-                        multiline={true}
-                        fullWidth={true}
+                    inputProps={{"aria-label":"naked", readOnly:this.state.readOnly}}
+                    type="text"
+                    id={item.id}
+                    name={item.id}
+                    value={item.title}
+                    multiline={true}
+                    fullWidth={true}                  
                     />
                 </ListItemText>
             </ListItem>
         );
     }
 }
+
 export default Todo;
